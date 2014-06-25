c1226592620d56ffcf57eee313dc2f5b98acd095
Denis Popov <akadan47@gmail.com>
2014-06-25 05:39:41 +0400

    Add InitialFieldsMixin

---
+++ django_extend/admin.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+from django.contrib import admin
+
+
+class InitialFieldsMixin(object):
+    def get_form(self, request, obj=None, **kwargs):
+        form = admin.ModelAdmin.get_form(self, request, obj, **kwargs)
+        if not hasattr(self.__class__, 'initial'):
+            return form
+        old_init = form.__init__
+
+        def new_init(_self, *args, **kwargs):
+            if 'instance' not in kwargs:
+                for field_name, callback in self.__class__.initial.iteritems():
+                    kwargs['initial'][field_name] = callback(self, request,
+                                                             obj, **kwargs)
+            return old_init(_self, *args, **kwargs)
+        form.__init__ = new_init
+
+        return form
