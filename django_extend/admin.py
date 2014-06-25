8b51ded39b12c20f7014bef06f1488f35a79fb2e
Denis Popov <akadan47@gmail.com>
2014-06-25 05:48:55 +0400

    Fix InitialFieldsMixin

--- django_extend/admin.py
+++ django_extend/admin.py
@@ -10,10 +10,9 @@ class InitialFieldsMixin(object):
         old_init = form.__init__
 
         def new_init(_self, *args, **kwargs):
-            if 'instance' not in kwargs:
+            if 'instance' not in kwargs and 'initial' in kwargs:
                 for field_name, callback in self.__class__.initial.iteritems():
-                    kwargs['initial'][field_name] = callback(self, request,
-                                                             obj, **kwargs)
+                    kwargs['initial'][field_name] = callback(self, request, obj, **kwargs)
             return old_init(_self, *args, **kwargs)
         form.__init__ = new_init
 
